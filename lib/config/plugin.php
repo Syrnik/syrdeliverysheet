bd8955b5fcdb9897c9fc800b4de700c39e8f0843
Serge Rodovnichenko <sergerod@gmail.com>
2014-09-25 23:26:09 +0400

    First commit

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Delivery Sheet Printform',
+  'icon' => 'img/syrdeliverysheet.gif',
+  'version' => '1.0.0',
+  'vendor' => '670917',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/shopSyrdeliverysheetPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopSyrdeliverysheetPlugin extends shopPlugin
+{
+
+}
