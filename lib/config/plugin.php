8281d3b7d19460c772539da1aed613f90e9c9d04
Serge Rodovnichenko <sergerod@gmail.com>
2015-05-11 19:10:46 +0300

    Version Number Increase

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -2,7 +2,7 @@
 return array (
     'name' => _wp('Delivery Sheet'),
     'img' => 'img/receipt-invoice.png',
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'vendor' => '670917',
     'frontend'    => FALSE,
     'printform' => TRUE,
